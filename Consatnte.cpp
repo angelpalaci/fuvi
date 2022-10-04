4d38fba6a472a1530d52942e64996342124020e5
Angel palacio <electronica2022@fuvi.net>
2022-10-04 10:16:25 -0600

    Se modifico el archivo constante.cpp y se agrego pitagoras.cpp

--- Consatnte.cpp
+++ Consatnte.cpp
@@ -1,1 +1,18 @@
-/* constante*/
+/* Nombre del archivo: constantes.cpp
+Autor: Angel Palacio
+Lugar: ITV
+Instrurcciones: Uso de las constantes
+*/
+#include<iostream>
+#include<cmath>
+using namespace std;
+//Variables globales
+const double pi = 3.141592;
+int main (){
+	
+	double radio, area;
+	cout<< "Ingrese el radio del circulo:";
+	cin >> radio;
+	area = pi * pow(radio,2);
+	cout<< "El area de un circulo con radio"<< radio<<"es"<<area;
+}
